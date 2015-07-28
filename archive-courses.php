efaf898ac7856acc02bf7a7968122404f609bcb7
NC Jones <nicollecjones@gmail.com>
2015-07-28 13:51:22 -0400

    Styles and Course display tweaks
    Modified some styles for course archive display loop. Added a
    session[cost] custom field to course cpt.

--- archive-courses.php
+++ archive-courses.php
@@ -8,39 +8,14 @@ get_header(); ?>
 		<div id="content-area" class="clearfix">
 			<div id="left-area">
 
-				<h3>All upcoming Courses</h3>
-
-				<!-- FILTER BY TAXO or CUSTOM FIELD OPTIONS -->
-
-				<?php
-
-				/* SOMEHOW GET $LOCATION via pulldown? */
-				$LOCATION = 'slug';
-
-				/*BUILD ARRAY by $LOCATION */
-				$courseby_location = get_posts(array(
-				  'post_type' => 'courses',
-				  'numberposts' => -1,
-				  'tax_query' => array(
-				    array(
-				      'taxonomy' => 'location',
-				      'field' => 'slug',
-				      'terms' => $LOCATION , // Where $LOCATION is loaded with slug
-				      'include_children' => true //True because only region location values would have children; cities should be implicit
-				    )
-				  )
-				));
-
-				?>
-
-				<p>OUTPUT SOME LINKS TO FILTER BY</p>
-
+				<h1>All IES Courses</h1>
 				<article>
+					<div id="course-print">
 				<?php
 
 					 $args = array( 'post_type' => 'courses',
 						
-						'posts_per_page' => 20,
+						'posts_per_page' => 30,
 						'order' => 'ASC',
 					 );
 					 $the_courses = new WP_Query( $args );
@@ -48,22 +23,23 @@ get_header(); ?>
 					if ( $the_courses->have_posts() ) :
 
 
-						echo '<ul>';
+						echo '<ul id="ies_course_table">';
 						while ( $the_courses->have_posts() ) : $the_courses->the_post();
 							$ies_instructors = get_post_meta($post->ID, 'instructor', true);
 							$rows = get_field('sessions');
 							
-							echo '<br /><li style="list-style-type: none;"><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
-							echo '<table style="width:80%">';
+							echo '<br /><li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+							echo '<table id="ies_course_table">';
 
 							if ($rows) {
 								foreach($rows as $row) {
 								  $loc_id = get_term_by('id', $row['location'], 'location');
+								  $cost = $row['cost'];
 								  $name_id = $loc_id->name;
 								  $start_output = DateTime::createFromFormat('Ymd', $row['start_date'])->format('F d, Y');
 								  $end_output = DateTime::createFromFormat('Ymd', $row['end_date'])->format('F d, Y');
 								  $register_url = get_post_meta($post->ID, 'registration_link', true);
-								  echo '<tr><td> Location: ' . $name_id . '</td><td> Date: ' . $start_output . '</td><td>Cost: To be added&nbsp;</td></tr>';
+								  echo '<tr><td> Location: ' . $name_id . '</td><td> Date: ' . $start_output . '</td><td>Cost: ' . $cost . '</td></tr>';
 								}
 							}
 
@@ -78,7 +54,7 @@ get_header(); ?>
 					endif;
 				?>
 
-
+					</div>
 				</article>
 
 
---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php 
+
+/*
+IES Divi Child Theme for ies.ncsu.edu
+Description: Site specific code changes for ies.ncsu.edu
+Author: ncjones4@ncsu.edu
+*/
+
+// Custom Function to Include
+function favicon_link() {
+    echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "\n";
+}
+add_action( 'wp_head', 'favicon_link' );
+
+
+// import parent style.css
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+
+
