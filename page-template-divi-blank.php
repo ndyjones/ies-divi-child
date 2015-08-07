713f167f473a8a3972171cf82884239ece41ffd2
NC Jones <nicollecjones@gmail.com>
2015-08-07 10:05:34 -0400

    Changed blank page templates
    Modified a Divi Blank template to drop some elements and added a
    completely blank wp page template with NCSU brand CSS, Bootstrap JS and
    jQuery.

---
+++ page-template-divi-blank.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Template Name: Divi Blank with Nav
+*/
+
+get_header(); ?>
+
+  
+
+<div id="main-content">
+
+	<div class="container">
+		<div id="content-area" class="clearfix">
+			<div id="left-area">
+
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<?php if ( ! $is_page_builder_used ) : ?>
+
+					<h1 class="main_title"><?php the_title(); ?></h1>
+				<?php
+					$thumb = '';
+
+					$width = (int) apply_filters( 'et_pb_index_blog_image_width', 1080 );
+
+					$height = (int) apply_filters( 'et_pb_index_blog_image_height', 675 );
+					$classtext = 'et_featured_image';
+					$titletext = get_the_title();
+					$thumbnail = get_thumbnail( $width, $height, $classtext, $titletext, $titletext, false, 'Blogimage' );
+					$thumb = $thumbnail["thumb"];
+
+					if ( 'on' === et_get_option( 'divi_page_thumbnails', 'false' ) && '' !== $thumb )
+						print_thumbnail( $thumb, $thumbnail["use_timthumb"], $titletext, $width, $height );
+				?>
+
+				<?php endif; ?>
+
+					<div class="entry-content">
+					
+					<?php the_content(); ?>
+					
+					</div> <!-- .entry-content -->
+
+				
+				</article> <!-- .et_pb_post -->
+
+			<?php endwhile; ?>
+
+<?php if ( ! $is_page_builder_used ) : ?>
+
+			</div> <!-- #left-area -->
+
+			<?php get_sidebar(); ?>
+		</div> <!-- #content-area -->
+	</div> <!-- .container -->
+
+<?php endif; ?>
+
+</div> <!-- #main-content -->
+
+<?php get_footer(); ?>
