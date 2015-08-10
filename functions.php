3147dfd7691c40b3cf2969cf71c5d4b2d5f2e063
NC Jones <nicollecjones@gmail.com>
2015-08-10 16:20:35 -0400

    Style tweaks for NC State branding alignment
    Some style tweaks to Divi elements and added nc state brand favicon to
    functions.php

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ Author: ncjones4@ncsu.edu
 
 // Custom Function to Include
 function favicon_link() {
-    echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "\n";
+    echo '<link rel="shortcut icon" type="image/x-icon" href="http://www.ncsu.edu/favicon.ico" />' . "\n";
 }
 add_action( 'wp_head', 'favicon_link' );
 
